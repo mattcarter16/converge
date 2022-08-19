ad5f52f8571af064cb27609ca4b51f2fe241c520
MattCarterValorem <MattCarterValorem@beamsuntory.com>
2022-08-19 07:39:13 -0500

    added userId to telemetry

--- Converge/Services/PredictionService.cs
+++ Converge/Services/PredictionService.cs
@@ -74,7 +74,7 @@ namespace Converge.Services
                     var start = predictionWindowList[i].Start;
                     var end = predictionWindowList[i].End;
                     Location predictedLocation = GetPredictedLocation(start, end, eventsList, placesDictionary, out DateTimeOffset? lastWorkspaceBookingModified);
-                    telemetryService.TrackEvent("Get predicted location", "predictedLocation", predictedLocation);
+                    telemetryService.TrackEvent("Get predicted location" + userId, "predictedLocation", predictedLocation);
                     await CreateOrUpdatePrediction(userId, predictedLocation, TimeZoneInfo.ConvertTimeBySystemTimeZoneId(start, workingHours.TimeZone.Name), false, lastWorkspaceBookingModified);
                 }
                 catch (Exception e)
